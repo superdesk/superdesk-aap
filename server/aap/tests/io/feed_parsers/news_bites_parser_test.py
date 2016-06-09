7d542476dd594b24c14f89abf377c76a1ec0ded5
marwoodandrew <amarwood@aap.com.au>
2016-06-09 11:35:53 +1000

    [SD-4425] Add parser for News Bites

---
+++ server/aap/tests/io/feed_parsers/news_bites_parser_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+
+import os
+from test_factory import SuperdeskTestCase
+from aap.io.feed_parsers.news_bites import NewsBitesFeedParser
+
+
+class NewsBitesFileTestCase(SuperdeskTestCase):
+
+    filename = 'AAP201606060716291.tst'
+
+    def setUp(self):
+        super().setUp()
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        self.fixture = os.path.normpath(os.path.join(dirname, '../fixtures', self.filename))
+        self.provider = {'name': 'Test'}
+
+    def test_headline(self):
+        item = NewsBitesFeedParser().parse(self.fixture, self.provider)
+        self.assertEqual(item['headline'], 'UGL continues project delay negotiation')
+        self.assertEqual(item['format'], 'HTML')
+        # Note left sloping apostrophe in string below, causes grief in html, use the
+        # ruleset function to replace it.
+        self.assertTrue(item['body_html'].startswith('<p>UGL’s construction'))
---
+++ server/aap/tests/io/fixtures/AAP201606060716291.tst
@@ -0,0 +1,15 @@
+zczc
+AN#26945219
+KN#News Bites
+PD#06 June 2016 14:00:00
+CO#UGL.AX
+SO#ASX
+SF#*
+BY#
+KY#ACNB Comnews UGL.AX
+TI#UGL continues project delay negotiation
+AB#UGL’s construction of the Ichthys SMP and CCPP projects continues to experience substantial delays and disruption.
+TX#
+UGL’s construction of the Ichthys SMP and CCPP projects (in joint venture with Kentz and CH2M Hill respectively) continues to experience substantial delays and disruption attributable to the client, JKC (a JV between JGC, KBR and Chiyoda).
+Substantial claims have been raised with JKC on both SMP and CCPP. UGL maintains a strong contractual position with issues clearly attributable to client delays and disruption.
+Substantive commercial negotiation of the SMP claims is ongoing however, negotiations have become protracted. As previously stated, margin will not be recognised on the SMP project while commercial negotiations are ongoing.
