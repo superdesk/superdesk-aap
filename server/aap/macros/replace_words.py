64c440c198b4fa08c0909557c17b7003348f659d
marwoodandrew <amarwood@aap.com.au>
2016-06-20 17:15:00 +1000

    [SD-4728] Attempt to match the case of the original text in the Replace American Words macro

--- server/aap/macros/replace_words.py
+++ server/aap/macros/replace_words.py
@@ -22,13 +22,52 @@ def find_and_replace(item, **kwargs):
     """
     diff = {}
 
+    def repl(new, old):
+        """
+        Returns a version of the "new" string that matches the case of the "old" string
+        :param new:
+        :param old:
+        :return: a string which is a version of "new" that matches the case of old.
+        """
+        if old.islower():
+            return new.lower()
+        elif old.isupper():
+            return new.upper()
+        else:
+            # the old string starts with upper case so we use the title function
+            if old[:1].isupper():
+                return new.title()
+            # it is more complex so try to match it
+            else:
+                result = ''
+                all_upper = True
+                for i, c in enumerate(old):
+                    if i >= len(new):
+                        break
+                    if c.isupper():
+                        result += new[i].upper()
+                    else:
+                        result += new[i].lower()
+                        all_upper = False
+                # append any remaining characters from new
+                if all_upper:
+                    result += new[i + 1:].upper()
+                else:
+                    result += new[i + 1:].lower()
+                return result
+
     def do_find_replace(input_string, words_list):
         for word in words_list:
             pattern = r'{}'.format(re.escape(word.get('existing', '')))
 
-            if re.search(pattern, input_string, flags=re.IGNORECASE):
-                diff[word.get('existing', '')] = word.get('replacement', '')
-                input_string = re.sub(pattern, word.get('replacement', ''), input_string, flags=re.IGNORECASE)
+            while re.search(pattern, input_string, flags=re.IGNORECASE):
+                # get the original string from the input
+                original = re.search(pattern, input_string, flags=re.IGNORECASE).group(0)
+                replacement = repl(word.get('replacement', ''), original)
+                if original == replacement:
+                    break
+                diff[original] = replacement
+                input_string = input_string.replace(original, replacement)
 
         return input_string
 
