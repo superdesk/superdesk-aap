31ec85303dff5f9b73f292c9a9bfd280d3406bdc
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2016-09-13 15:27:20 +1000

    Avoid duplicate locators in headline for auto publishing content with locators in headline.

--- server/aap/publish/formatters/aap_ipnews_formatter_test.py
+++ server/aap/publish/formatters/aap_ipnews_formatter_test.py
@@ -745,6 +745,52 @@ class AapIpNewsFormatterTest(TestCase):
         item = json.loads(item)
         self.assertEqual(item['wordcount'], 0)
 
+    def testHeadlinePrefix(self):
+        article = {
+            '_id': '3',
+            'source': 'AAP',
+            'anpa_category': [{'qcode': 's'}],
+            'headline': 'This is a test headline',
+            'byline': 'joe',
+            'slugline': 'slugline',
+            'subject': [{'qcode': '15063000'}],
+            'anpa_take_key': 'take_key',
+            'unique_id': '1',
+            'type': 'text',
+            'body_html': '<p>Test</p>',
+            'word_count': 150,
+            'priority': 1,
+        }
+        subscriber = self.app.data.find('subscribers', None, None)[0]
+
+        f = AAPIpNewsFormatter()
+        seq, item = f.format(article, subscriber)[0]
+        item = json.loads(item)
+        self.assertEqual(item['headline'], 'TTEN:This is a test headline')
+
+    def testHeadlinePrefixLegacy(self):
+        article = {
+            '_id': '3',
+            'source': 'AAP',
+            'anpa_category': [{'qcode': 's'}],
+            'headline': 'ABCDEF:This is a test headline',
+            'byline': 'joe',
+            'slugline': 'slugline',
+            'subject': [{'qcode': '15063000'}],
+            'anpa_take_key': 'take_key',
+            'unique_id': '1',
+            'type': 'text',
+            'body_html': '<p>Test</p>',
+            'word_count': 150,
+            'priority': 1,
+        }
+        subscriber = self.app.data.find('subscribers', None, None)[0]
+
+        f = AAPIpNewsFormatter()
+        seq, item = f.format(article, subscriber)[0]
+        item = json.loads(item)
+        self.assertEqual(item['headline'], 'TTEN:ABCDEF:This is a test headline')
+
 
 class DefaultSubjectTest(TestCase):
 
