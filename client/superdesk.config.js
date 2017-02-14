0e7f5ba40bcaa484314407d7260afe75efbc8e70
Tolga Akin <akint@aap.com.au>
2017-02-14 20:41:13 +1100

    Adding the queue error field

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -81,6 +81,7 @@ module.exports = function() {
                 'slugline',
                 'highlights',
                 'associations',
+                'queueError',
                 'headline',
                 'markedDesks',
                 'versioncreated'
