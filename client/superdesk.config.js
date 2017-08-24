acef2c2b3b0f9485d8c86481304579ce65978c51
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2017-08-24 13:16:59 +1000

    Enable the single line view user preference.

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -128,7 +128,7 @@ module.exports = function() {
                 'provider',
                 'versioncreated'
             ],
-            'singleLineView': false
+            'singleLineView': true
         },
         langOverride: {
             'en': {
