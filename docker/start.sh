38c7c21f3180c27b1856a258ce7c69e7c810ebb0
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2018-03-14 12:42:17 +1100

    Fixing the build script.

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/superdesk/client/dist &&
-sed -i -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" app*.js -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/" &&
+sed -i -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" app*.js -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" &&
 nginx &
 
 cd /opt/superdesk &&
