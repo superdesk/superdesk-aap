f63816433470deda1f3c08214f27f41977a13a14
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2018-06-21 17:28:07 +1000

    [SDESK-3074] AM Service macro to set the genre.

---
+++ server/aap/macros/am_service_content_test.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import unittest
+from copy import deepcopy
+from .am_service_content import am_service_content
+
+
+class AMServiceTestCase(unittest.TestCase):
+
+    def test_genre_not_specified(self):
+        item = {'body_html': 'text', 'type': 'text'}
+        modified_item = am_service_content(deepcopy(item))
+        self.assertEqual(modified_item['genre'][0], {'name': 'AM Service', 'qcode': 'AM Service'})
+        self.assertNotIn('genre', item)
+
+    def test_genre_specified(self):
+        item = {'body_html': 'text', 'type': 'text', 'genre': [{'name': 'Article', 'qcode': 'Article'}]}
+        modified_item = am_service_content(deepcopy(item))
+        self.assertEqual(modified_item['genre'][0], {'name': 'AM Service', 'qcode': 'AM Service'})
+        self.assertEqual(item['genre'][0], {'name': 'Article', 'qcode': 'Article'})
+
+    def test_not_type_text(self):
+        item = {'body_html': 'text', 'type': 'picture', 'genre': [{'name': 'Article', 'qcode': 'Article'}]}
+        modified_item = am_service_content(deepcopy(item))
+        self.assertEqual(modified_item['genre'][0], {'name': 'Article', 'qcode': 'Article'})
+        self.assertEqual(item['genre'][0], {'name': 'Article', 'qcode': 'Article'})
