e47d70237184cc2c21d13c7ce9c0a1a5fde459e7
Tolga Akin <akint@aap.com.au>
2019-04-29 22:22:00 +1000

    [SWP-1525] - Setting the default category per environment variable

--- server/settings.py
+++ server/settings.py
@@ -193,6 +193,9 @@ DEFAULT_URGENCY_VALUE_FOR_MANUAL_ARTICLES = int(env('DEFAULT_URGENCY_VALUE_FOR_M
 DEFAULT_GENRE_VALUE_FOR_MANUAL_ARTICLES = [{'qcode': 'Article', 'name': 'Article'}]
 RESET_PRIORITY_VALUE_FOR_UPDATE_ARTICLES = json.loads(env('RESET_PRIORITY_VALUE_FOR_UPDATE_ARTICLES', 'True').lower())
 
+#: Defines default value for category for ingested and auto published articles
+DEFAULT_CATEGORY_VALUE_FOR_AUTO_PUBLISHED_ARTICLES = [{'qcode': 'a', 'name': 'Australian General News'}]
+
 # This value gets injected into NewsML 1.2 and G2 output documents.
 NEWSML_PROVIDER_ID = 'aap.com.au'
 ORGANIZATION_NAME = 'Australian Associated Press'
