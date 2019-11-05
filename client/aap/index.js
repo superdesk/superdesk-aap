0f7b00757dd73156ca05c854fd7c89b3498a3bb8
Tomas Kikutis <t.kikutis@gmail.com>
2019-11-05 12:14:56 +0100

    enable planning extension

--- client/aap/index.js
+++ client/aap/index.js
@@ -2,6 +2,16 @@ import './publish';
 import './mission_report';
 import './sms_report';
 
+import {startApp} from 'superdesk-core/scripts/index';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/src/extension';
+
+setTimeout(() => {
+    startApp(
+        [planningExtension],
+        {},
+    );
+});
+
 export default angular.module('aap.apps', [
     'aap.apps.publish',
     'aap.apps.mission-report',
