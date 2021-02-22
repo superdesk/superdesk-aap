39f1d91da74bf7075a18a7b8c62834d8f15dad8b
marwoodandrew <amarwood@aap.com.au>
2021-02-22 11:46:36 +1100

    Dont expand urls if editor 3

--- server/aap/publish/formatters/aap_newsroom_ninjs_formatter.py
+++ server/aap/publish/formatters/aap_newsroom_ninjs_formatter.py
@@ -58,7 +58,7 @@ class AAPNewsroomNinjsFormatter(NewsroomNinjsFormatter):
                 'ingest_provider') and dc_match:
             ninjs[GUID_FIELD] = article.get('ingest_id')
 
-        if article.get(FORMAT) == FORMATS.HTML:
+        if article.get(FORMAT) == FORMATS.HTML and 'fields_meta' not in article:
             ninjs['body_html'] = self._format_url_to_anchor_tag(ninjs.get('body_html', ''))
 
         # if the article has an abstract then the description text has been over written by the abstract
