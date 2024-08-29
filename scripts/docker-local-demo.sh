86a28b11aabd70451f6b4f19b9a606c333de640c
marwoodandrew <amarwood@aap.com.au>
2024-08-29 15:28:25 +1000

    fix docker compose change:

--- scripts/docker-local-demo.sh
+++ scripts/docker-local-demo.sh
@@ -3,7 +3,7 @@
 set -ue
 
 function dcs() {
-	docker-compose -p sddemo -f ./docker-compose-prebuilt.yml $@
+	docker compose -p sddemo -f ./docker-compose-prebuilt.yml $@
 }
 
 WORK_DIR="$(echo $(cd -P -- "$(dirname -- "$0")" && pwd -P))"
